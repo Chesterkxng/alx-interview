826a6005045197edfd2603812d3dd788179ea6aa
Chesterkxng <cgoita00@gmail.com>
2024-05-07 10:30:11 +0000

    edges case

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,18 +8,18 @@ def minOperations(n: int) -> int:
     """
     Minimum Operations
     """
-    if n <= 0:
+    if (not isinstance(n, int) or  n <= 1):
         return 0
     result: string = "H"
     tmp: string = ""
     num_operations: int = 0
     while len(result) < n:
-        if n % len(result) == 0:
+        if (n % len(result)) == 0:
             tmp = result
             result += tmp
             num_operations += 2
             continue
-        elif n % len(result) != 0:
+        elif (n % len(result)) != 0:
             result += tmp
             num_operations += 1
             continue
