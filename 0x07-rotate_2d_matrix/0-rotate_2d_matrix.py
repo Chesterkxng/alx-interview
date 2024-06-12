25e9449822a3dcc55839813d48bfe4e6a442e639
Chesterkxng <cgoita00@gmail.com>
2024-06-12 15:15:39 +0000

    rotate matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+MATRIX ROTATION
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[int]):
+    """    ROTATE 2D MATRIX
+    """
+    dict = {}
+    n: int = len(matrix)
+    i = 0
+    k = 0
+    while i < n:
+        row: List[int] = []
+        for j in range(n - 1, -1, -1):
+            row.append(matrix[j][k])
+        dict[k] = row
+        k = k + 1
+        i = i + 1
+    for key, val in dict.items():
+        matrix[key] = val
