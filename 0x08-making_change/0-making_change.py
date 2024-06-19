2f3621ff3315a59d73335a6ca53fa00f2e9f110e
Chesterkxng <cgoita00@gmail.com>
2024-06-19 19:40:49 +0000

    chnges

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+COINS CHANGE
+"""
+
+
+def makeChange(coins, total):
+    """
+    algo
+    """
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
